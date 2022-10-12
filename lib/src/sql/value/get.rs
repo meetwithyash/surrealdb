069796da35298c6c32e7412449e86aa9d178e3d0
unknown <ykhokhaneshiy@gmail.com>
2022-10-12 12:50:17 +0530

    testing first change

--- lib/src/sql/value/get.rs
+++ lib/src/sql/value/get.rs
@@ -43,7 +43,7 @@ impl Value {
 					Part::All => {
 						let path = path.next();
 						let futs = v.iter().map(|v| v.get(ctx, opt, txn, path));
-						stream::iter(futs).buffered(10).collect().await.map(Into::into)
+						stream::iter(futs).buffered(10).collect::<C>().await.map(Into::into)
 					}
 					Part::First => match v.first() {
 						Some(v) => v.get(ctx, opt, txn, path.next()).await,
